296896b206e889a8c3c74404b8d09cc7cf86f9f8
EdvinN1 <nedde14@gmail.com>
2023-12-04 15:16:51 +0100

    added delete-advert method. Also added some comments

--- src/main/java/com/springboot/blocket/controllers/AdvertController.java
+++ src/main/java/com/springboot/blocket/controllers/AdvertController.java
@@ -4,6 +4,7 @@ import com.springboot.blocket.dtos.UpdateAdvertDto;
 import com.springboot.blocket.models.Advert;
 import com.springboot.blocket.services.AdvertService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +27,7 @@ public class AdvertController {
         Advert createdAdvert = advertService.createAdvert(advert, token);
         return ResponseEntity.ok(createdAdvert);
     }
+    //Return a list of all  adverts, yours and others
     @GetMapping("/advert/all")
     public ResponseEntity<List<Advert>> getAllAdverts() {
         List<Advert> adverts = advertService.getAllAdverts();
@@ -39,13 +41,13 @@ public class AdvertController {
         Advert updatedAdvert = advertService.updateAdvert(id, updatedAdvertDto, token);
         return ResponseEntity.ok(updatedAdvert);
     }
-
+    //Return a specfic advert to get information about it
     @GetMapping("/advert/{id}")
     public ResponseEntity<Advert> getAdvertById(@PathVariable int id) {
         Optional<Advert> advert = advertService.getAdvertById(id);
         return advert.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
-    
+    //Return a list of adverts with a specific category
     @GetMapping("/advert/by-category/{category}")
     public ResponseEntity<List<Advert>> getAdvertsByCategory(@PathVariable String category) {
         List<Advert> adverts = advertService.getAdvertsByCategory(category);
@@ -56,5 +58,16 @@ public class AdvertController {
     @GetMapping("/advert/user/show-all")
     public ResponseEntity<List<Advert>> getAllUserAdverts(@RequestHeader("Authorization") String token){
         return ResponseEntity.ok(advertService.getAllUserAdverts(token));
+    }
+    //Delete an advert you created, authorized with token
+    @DeleteMapping("/advert/delete/{id}")
+    public ResponseEntity<String> deleteAdvert(@PathVariable int id,
+                                               @RequestHeader("Authorization") String token) {
+        try {
+            advertService.deleteAdvert(id, token);
+            return ResponseEntity.ok("Advert deleted successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
     }
 }
